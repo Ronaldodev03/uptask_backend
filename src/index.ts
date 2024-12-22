187f5d9c07c9e5a6a807d0405d4df37da5601291
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-22 18:55:56 -0400

    add color msg dependency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import server from "./server.js";
+import colors from "colors";
+import server from "./server";
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`REST API funcionando en el puerto ${port}`);
+  console.log(colors.bgGreen.bold(`REST API funcionando en el puerto ${port}`));
 });
