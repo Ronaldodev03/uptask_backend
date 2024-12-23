ac5f3ac425a9f1a5f5bf66eaace19d6a99ef30fe
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-22 22:03:03 -0400

    add validation to inputs in Projects using express validator + middleware for errors in inputs

---
+++ src/middleware/validation.ts
@@ -0,0 +1,16 @@
+import type { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+
+export const handleInputErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+  next(); // con esto se sigue al siguiente middleware.
+  return;
+};
