f6db618f022049ed2a11d3cdbc5c847cf1d48377
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-22 22:01:03 -0400

    add Projects: model, routes and controller (starting)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import projectRoutes from "./routes/ProjectRoutes";
 
 dotenv.config(); // para usar variables de entorno
 
 connectDB();
 
 const app = express();
+
+app.use(express.json()); // Leer datos del body en json (se envian desde forms, por ejemplo)
+
+// Routes
+app.use("/api/projects", projectRoutes);
 
 export default app;
