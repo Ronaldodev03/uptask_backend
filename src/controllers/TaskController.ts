1e333cc3cb93e482eeee36b3ca7c3ea672f704c4
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-23 17:56:03 -0400

    add delete task & refactor get task & update task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -30,31 +30,7 @@ export class TaskController {
   static getTaskById = async (req: Request, res: Response) => {
     const { taskId } = req.params;
     try {
-      const task = await Task.findById(taskId).populate("project"); // con populate me traigo la info del project
-
-      if (!task) {
-        const error = new Error("Tarea no encontrada");
-        res.status(404).json({ error: error.message });
-        return;
-      }
-
-      // la task debe pertenecer al project
-      if (task.project.id.toString() !== req.project.id) {
-        const error = new Error("Accion no válida");
-        res.status(400).json({ error: error.message });
-        return;
-      }
-
-      res.json(task);
-    } catch (error) {
-      res.status(500).json({ error: "Hubo un error" });
-    }
-  };
-
-  static updateTask = async (req: Request, res: Response) => {
-    const { taskId } = req.params;
-    try {
-      const task = await Task.findByIdAndUpdate(taskId, req.body);
+      const task = await Task.findById(taskId);
 
       if (!task) {
         const error = new Error("Tarea no encontrada");
@@ -69,9 +45,67 @@ export class TaskController {
         return;
       }
 
+      res.json(task);
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
+
+  static updateTask = async (req: Request, res: Response) => {
+    const { taskId } = req.params;
+    try {
+      const task = await Task.findById(taskId);
+
+      if (!task) {
+        const error = new Error("Tarea no encontrada");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      // la task debe pertenecer al project
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error("Accion no válida");
+        res.status(400).json({ error: error.message });
+        return;
+      }
+
+      task.name = req.body.name;
+      task.description = req.body.description;
+      await task.save();
       res.send("Tarea Actualizada Correctamente");
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
+    }
+  };
+
+  static deleteTask = async (req: Request, res: Response) => {
+    const { taskId } = req.params;
+
+    try {
+      const task = await Task.findById(taskId);
+
+      if (!task) {
+        const error = new Error("Tarea no encontrada");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      // la task debe pertenecer al project
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error("Accion no válida");
+        res.status(400).json({ error: error.message });
+        return;
+      }
+
+      req.project.tasks = req.project.tasks.filter(
+        (task) => task.toString() !== taskId
+      );
+
+      await Promise.all([task.deleteOne(), req.project.save()]);
+
+      res.send("Tarea Eliminada Correctamente");
+    } catch (error) {
+      console.log(error);
     }
   };
 }
