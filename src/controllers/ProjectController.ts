d7f8b2b877656df2a00fec0cba01142c395a9e5e
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-22 22:51:32 -0400

    add delete project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -56,4 +56,24 @@ export class ProjectController {
       console.log(error);
     }
   };
+
+  static deleteProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      await project.deleteOne();
+
+      res.send("Proyecto Eliminado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
