a16f777bf8ce44ae95a46259532ec643cee5d39a
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-23 00:00:32 -0400

    create task & saving it also in its project

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from "express";
+import Task from "../models/Task";
+import Project from "../models/Project";
+
+export class TaskController {
+  static createTask = async (req: Request, res: Response) => {
+    const { projectId } = req.params;
+
+    try {
+      const project = await Project.findById(projectId);
+
+      if (!project) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      const task = new Task(req.body);
+      task.project = project.id;
+      project.tasks.push(task.id);
+
+      await task.save();
+      await project.save();
+
+      res.send("Tarea creada correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
+}
--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { body, param } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputErrors } from "../middleware/validation";
+import { TaskController } from "../controllers/TaskController";
 
 const router = Router();
 
@@ -50,6 +51,27 @@ router.delete(
   param("id").isMongoId().withMessage("ID no válido"),
   handleInputErrors,
   ProjectController.deleteProject
+);
+
+/* ------------------------------------------------------------------- */
+
+/** Routes for tasks */
+
+router.post(
+  "/:projectId/tasks",
+  body("name").notEmpty().withMessage("El Nombre de la tarea es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria"),
+  handleInputErrors,
+  TaskController.createTask
+);
+
+router.get(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido"),
+  handleInputErrors,
+  ProjectController.getProjectById
 );
 
 export default router;
