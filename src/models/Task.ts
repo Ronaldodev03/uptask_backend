ba34c930c2bd9f68b3cca97f525731ce5d53a0a1
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-22 22:59:20 -0400

    add model for Task

---
+++ src/models/Task.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ITask extends Document {
+  name: string;
+  description: string;
+}
+
+export const TaskSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model<ITask>("Task", TaskSchema);
+export default Task;
