1cd352e0db561ef36df37699a374aada34d4e797
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-27 16:01:49 -0400

    add auth (model for user, controller & route) & dependencies for encoding password & script for testing endpoint in thunderclient & createAccount method

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
+import authRoutes from "./routes/AuthRoutes";
 import projectRoutes from "./routes/ProjectRoutes";
 
 dotenv.config(); // para usar variables de entorno
@@ -16,6 +17,7 @@ app.use(cors(corsConfig));
 app.use(express.json()); // Leer datos del body en json (se envian desde forms, por ejemplo)
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
 export default app;
