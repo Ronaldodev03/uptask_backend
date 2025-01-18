7e6e507288a9215dcb66adceeac061e4f14d7b29
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-01-18 15:27:13 -0400

    add gmail support

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -2,13 +2,23 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const config = () => {
+//   return {
+//     host: process.env.SMTP_HOST,
+//     port: +process.env.SMTP_PORT,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//   };
+// };
+
 const config = () => {
   return {
-    host: process.env.SMTP_HOST,
-    port: +process.env.SMTP_PORT,
+    service: "Gmail",
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   };
 };
